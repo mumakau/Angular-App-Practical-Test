03b0613990e1d2e2882983f559a3b3050941f3b1
mumakau <mukiimakau@gmail.com>
2022-03-29 03:10:40 +0300

    server edits

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./front-end/dist/angular-app-practical-test'));
-app.get('/*', function(req,res) {res.sendFile(path.join('./front-end/dist/angular-app-practical-test/index.html'));
+app.use(express.static('./dist/angular-app-practical-test'));
+app.get('/*', function(req,res) {res.sendFile(path.join('./dist/angular-app-practical-test/index.html'));
 });
 app.listen(process.env.PORT || 8080);
