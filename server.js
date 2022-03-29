db799bac1025bd00a15d5912d6ff148b39635d60
mumakau <mukiimakau@gmail.com>
2022-03-29 04:06:05 +0300

    server edits

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/angular-app-practical-test'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/angular-app-practical-test/'});
+app.use(express.static(__dirname + '/dist/angular-app-practical-test'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-app-practical-test/index.html'));
 });
 app.listen(process.env.PORT || 8080);
