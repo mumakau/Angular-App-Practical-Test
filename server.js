c0c76d9ccfdfa53a390e6b16ba8eb4b17b0170be
mumakau <mukiimakau@gmail.com>
2022-03-29 10:54:37 +0300

    ready for deployment

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/angular-app-practical-test'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-app-practical-test/index.html'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-app-practical-test/src/index.html'));
 });
 app.listen(process.env.PORT || 8080);
